922e0f709e2aa7d9d60048531bd6972345783f44
colaking69 <ogoritoo@gmail.com>
2024-10-26 17:38:27 +0300

    stage 3 text area length

--- Client/src/app/realestate/components/publish/estate-stage3/estate-stage3.component.ts
+++ Client/src/app/realestate/components/publish/estate-stage3/estate-stage3.component.ts
@@ -4,47 +4,73 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-estate-stage3',
   templateUrl: './estate-stage3.component.html',
-  styleUrl: './estate-stage3.component.scss'
+  styleUrl: './estate-stage3.component.scss',
 })
-export class EstateStage3Component implements OnInit{
-  @Input() show!: boolean
-  @Input() data: undefined | {}
-  @Output() formCompleted = new EventEmitter<{type:number}>()
+export class EstateStage3Component implements OnInit {
+  @Input() show!: boolean;
+  @Input() data: undefined | {};
+  @Output() formCompleted = new EventEmitter<{ type: number }>();
 
   roomsList = [
-    "0","1", "1.5", "2", "2.5", "3", "3.5", "4", "4.5", "5", "5.5", "6", "6.5", "7", "7.5", "8", "8.5", "9", "9.5", "10", "10.5", "11", "11.5", "12", "12.5"
-  ]
-  showerRoomsList = [
-    "1", "2", "3", "4+"
-  ]
-  parkingList = [   
-    "ללא","1","2","3"
-  ]
-  balconyList = [
-    "ללא","1","2","3","4"
-  ]
+    '0',
+    '1',
+    '1.5',
+    '2',
+    '2.5',
+    '3',
+    '3.5',
+    '4',
+    '4.5',
+    '5',
+    '5.5',
+    '6',
+    '6.5',
+    '7',
+    '7.5',
+    '8',
+    '8.5',
+    '9',
+    '9.5',
+    '10',
+    '10.5',
+    '11',
+    '11.5',
+    '12',
+    '12.5',
+  ];
+  showerRoomsList = ['1', '2', '3', '4+'];
+  parkingList = ['ללא', '1', '2', '3'];
+  balconyList = ['ללא', '1', '2', '3', '4'];
 
-  constructor(
-    private fb : FormBuilder
-  ){}
-  form!: FormGroup
+  furnitureDetailsMaxLength = 200;
+  furnitureDetailsValue = '';
+  detailsMaxLength = 400;
+  detailsValue = '';
+
+  constructor(private fb: FormBuilder) {}
+  form!: FormGroup;
+  showFurnitureDetail = false;
+
   ngOnInit(): void {
     this.form = this.fb.group({
-      rooms:[],
-      showerRooms:[],
-      parking:[],
-      balcony:[],
-      features:[],
-      furnitureDetails:[],
-      details:[]
-    })
+      rooms: [],
+      showerRooms: [],
+      parking: [],
+      balcony: [],
+      features: [],
+      furnitureDetails: [],
+      details: [],
+    });
   }
 
-  onSubmit(){
-    console.log(this.form.valid)
-    console.log(this.form.value)
+  setFurnitureDetail(value: boolean) {
+    this.showFurnitureDetail = value;
+  }
 
-    if(this.form.valid)
-      this.formCompleted.emit(this.form.value)
+  onSubmit() {
+    console.log(this.form.valid);
+    console.log(this.form.value);
+
+    if (this.form.valid) this.formCompleted.emit(this.form.value);
   }
 }
--- Client/src/app/realestate/components/publish/property-feature/property-feature.component.ts
+++ Client/src/app/realestate/components/publish/property-feature/property-feature.component.ts
@@ -1,69 +1,93 @@
-import { AfterViewInit, ChangeDetectorRef, Component, OnInit, QueryList, TemplateRef, ViewChildren } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  QueryList,
+  TemplateRef,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-property-feature',
   templateUrl: './property-feature.component.html',
-  styleUrl: './property-feature.component.scss'
+  styleUrl: './property-feature.component.scss',
 })
 export class PropertyFeatureComponent implements AfterViewInit {
-  @ViewChildren('svg') svgsIcons!: QueryList<TemplateRef<any>>
-
-  icons: any = []
+  @ViewChildren('svg') svgsIcons!: QueryList<TemplateRef<any>>;
+  @Output() furnitureDetail = new EventEmitter<boolean>();
+  icons: any = [];
 
   featureList = [
     {
-      name:`גישה לנכים`,
-      selected: false,
-    },{
-      name:`מיזוג`,
-      selected: false,
-    },{
-      name:`סורגים`,
-      selected: false,
-    },{
-      name:`דוד שמש`,
-      selected: false,
-    },{
-      name:`מעלית`,
-      selected: false,
-    },{
-      name:`ריהוט`,
-      selected: false,
-    },{
-      name:`יחידת דיור`,
-      selected: false,
-    },{
-      name:`מטבח כשר`,
-      selected: false,
-    },{
-      name:`משופצת`,
-      selected: false,
-    },{
-      name:`ממ״ד`,
-      selected: false,
-    },{
-      name:`מחסן`,
-      selected: false,
-    },{
-      name:`דלתות רב-בריח`,
-      selected: false,
-    },{
-      name:`מזגן טורנדו`,
+      name: `גישה לנכים`,
       selected: false,
     },
-  ]
-  furniture = false
+    {
+      name: `מיזוג`,
+      selected: false,
+    },
+    {
+      name: `סורגים`,
+      selected: false,
+    },
+    {
+      name: `דוד שמש`,
+      selected: false,
+    },
+    {
+      name: `מעלית`,
+      selected: false,
+    },
+    {
+      name: `ריהוט`,
+      selected: false,
+    },
+    {
+      name: `יחידת דיור`,
+      selected: false,
+    },
+    {
+      name: `מטבח כשר`,
+      selected: false,
+    },
+    {
+      name: `משופצת`,
+      selected: false,
+    },
+    {
+      name: `ממ״ד`,
+      selected: false,
+    },
+    {
+      name: `מחסן`,
+      selected: false,
+    },
+    {
+      name: `דלתות רב-בריח`,
+      selected: false,
+    },
+    {
+      name: `מזגן טורנדו`,
+      selected: false,
+    },
+  ];
+  furniture = false;
 
   constructor(private cd: ChangeDetectorRef) {}
 
   ngAfterViewInit(): void {
-    this.icons = this.svgsIcons?.toArray()    
-    this.cd.detectChanges()
+    this.icons = this.svgsIcons?.toArray();
+    this.cd.detectChanges();
   }
 
-  selectMeFeature(index : number){
-    this.featureList[index].selected = !this.featureList[index].selected
-    if(index === 5)
-        this.furniture = !this.furniture
+  selectMeFeature(index: number) {
+    this.featureList[index].selected = !this.featureList[index].selected;
+    if (index === 5) this.furniture = !this.furniture;
+
+    if (this.featureList[index].name === 'ריהוט')
+      this.furnitureDetail.emit(this.featureList[index].selected);
   }
 }
